6f19f38dfb8a5e453e187773c15d838ed99963da
rinunu <rintaro.t@gmail.com>
2010-11-15 23:19:19 +0900

    Body のスペックを追加しました

--- media/spec/BodySpec.js
+++ media/spec/BodySpec.js
@@ -9,12 +9,24 @@ describe("Body", function() {
 	body.setParam('int', 100);
 	expect(body.param('int')).toEqual(100);
     });
-    
-    it('スキルを追加できる', function() {
-	expect(body.param('ice_magic_damage')).toEqual(0);
-	body.setSkill(dam.skills.MAGIC_ICE_MASTERY, 1);
-	var skill = body.skill(dam.skills.MAGIC_ICE_MASTERY);
-	expect(skill.rank()).toEqual(1);
-	expect(body.param('ice_magic_damage')).toEqual(0.15);
+
+    describe('スキル追加時', function(){
+        var skillBase;
+        beforeEach(function() {
+            skillBase = dam.skills.MAGIC_ICE_MASTERY;
+	    body.setSkill(skillBase, 1);
+        });
+
+        it('持っているスキルを取得できる', function() {
+	    var skill = body.skill(skillBase);
+            
+	    expect(skill.rank()).toEqual(1);
+	    expect(body.param('ice_magic_damage')).toEqual(0.15);
+        });
+        
+        it('パッシブ効果が追加される', function() {
+	    expect(body.param('ice_magic_damage')).toEqual(0.15);
+        });
+
     });
 });
